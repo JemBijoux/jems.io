a36d4a1d63c5f360191428ef4b860866a9511b93
Jem Bezooyen <jem@hipmedia.ca>
2018-09-16 20:52:04 -0700

    Add a basic homepage
    Step one: have a page that shows *something*, and the first page that we
    can show is the homepage. For now just put the site text onto the page
    in the center.
    
    - Add gatsby config that will tell our template to load emotion (and
    later include other gatsby plugins or conf)
    - Add to the .gitignore so we don't commit any generated gatsby files

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
+  ],
+}
